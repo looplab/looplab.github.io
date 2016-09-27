35aa18280349ab1a865bfa5a6b6e636afaec25e3
Max Ekman <max@looplab.se>
2016-09-27 12:02:31 +0200

    Move history out from Root to not recreate on hot reaload

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
 
 import configureStore from './store/configureStore'
 import Root from './Root'
 
 const store = configureStore()
+// Create an enhanced history that syncs navigation events with the store.
+const history = syncHistoryWithStore(browserHistory, store)
 
 render(
   <AppContainer>
-    <Root store={store} />
+    <Root store={store} history={history} />
   </AppContainer>,
   document.getElementById('root')
 )
@@ -20,7 +24,7 @@ if (module.hot) {
     const NextRoot = require('./Root').default
     render(
       <AppContainer>
-        <NextRoot store={store} />
+        <NextRoot store={store} history={history} />
       </AppContainer>,
       document.getElementById('root')
     )
